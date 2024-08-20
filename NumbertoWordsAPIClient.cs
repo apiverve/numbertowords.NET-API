ec5ad70d353a5a7662dc47e2c423b1d5d129685b
APIVerve Team <hello@apiverve.com>
2024-08-20 11:01:06 -0500

    Bug Fixes

--- NumbertoWordsAPIClient.cs
+++ NumbertoWordsAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, numbertowordsQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, NumbertoWordsQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(numbertowordsQueryOptions options = null)
+        public ResponseObj Execute(NumbertoWordsQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(numbertowordsQueryOptions options)
+        private string constructURL(NumbertoWordsQueryOptions options)
         {
             string url = _apiEndpoint;
 
